f632de52719d73f1f2a544375938a10e8affbdce
Michel B <michelbh@gol.com>
2017-10-10 14:07:33 +0900

    Initial Commit

---
+++ iOSStep3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSStep3
+//
+//  Created by Michel Bouchet on 2017/10/10.
+//  Copyright © 2017 Michel Bouchet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
